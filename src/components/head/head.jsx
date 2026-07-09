39557a1e169f4e3c849d12c80d0d60fef2b7e802
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-09 09:34:16 +0530

    sidebar/left navigation bar

---
+++ src/components/head/head.jsx
@@ -0,0 +1,20 @@
+import "./head.css";
+
+// props properties
+function Heading(props){
+//   const name = "Bhavadeep Reddy"
+  return(
+    <div className="announcement">
+      <h2 className="announcement-title">
+        📢 Announcement
+      </h2>
+
+      <p className="announcement-text">
+        Students, you are going to have a couple of placement drives in the upcoming days.
+      </p>
+    </div>
+  )
+}
+export default Heading;
+
+// we can also use destructuring 
