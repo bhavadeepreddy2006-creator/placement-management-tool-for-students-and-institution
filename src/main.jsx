377601f5ea7c99b132081e63488e541699f2297a
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-10 12:58:17 +0530

    Added BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-const div = <div>
-    <h1>Welcome to the project</h1>
-    <p>Learn today lead tomorow</p>
-  </div>
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   // <h1>Hello world</h1>
   // div
-  <StrictMode>
-    <App></App>
-  </StrictMode>
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
 )
