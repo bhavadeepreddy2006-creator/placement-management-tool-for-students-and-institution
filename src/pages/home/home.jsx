85ef502be9db3e4fd0e187b1a496a68de7534fa1
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-10 13:06:04 +0530

    home page

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,22 @@
+import "./home.css"
+import NavBar from "../../components/navbar/NavBar";
+import Heading from "../../components/head/head";
+import Sidebar from "../../components/sidebar/sidebar";
+import Dashbord from "../../components/dashbord/Dashbord";
+import Footer from "../../components/Footer/Footer";
+import Register from "../registration/Register";
+import Login from "../login/login";
+
+function Home(){
+    return(
+        <div>
+            <NavBar name = "Bhavadeep Reddy"/>
+            <div className="main">
+            <Sidebar/>
+            
+            </div>
+            <Footer/>
+        </div>
+    )
+}
+export default Home;
