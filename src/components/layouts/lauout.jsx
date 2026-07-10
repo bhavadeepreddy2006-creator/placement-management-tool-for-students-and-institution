03fba042c8a13aefa350a85cb54cd0ef8187ff5a
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-10 13:07:14 +0530

    Introduced Outlet to set layout

---
+++ src/components/layouts/lauout.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import NavBar from "../navbar/NavBar";
+import Sidebar from "../sidebar/sidebar";
+import "./layout.css"
+
+function Layout(){
+    return(
+        <>
+        <div className="body">
+            <NavBar/>
+            <div className="main">
+                <Sidebar/>
+                <div className="content">
+                    <Outlet/>
+                </div>
+            </div>
+            <Footer/>
+        </div>
+        </>
+    )
+}
+export default Layout;
