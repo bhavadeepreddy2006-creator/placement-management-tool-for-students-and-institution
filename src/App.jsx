e6c6af2fc709b71ddb2c2c00314084ba91174daf
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-10 12:55:59 +0530

    Added Route, Routes, Layout, using Link

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Login from "./pages/login/login";
-import Home from "./pages/home";
+import Home from "./pages/home/home";
+import Register from "./pages/registration/Register";
+import Studenttable from "./components/Studenttable";
+import { Route, Routes } from "react-router-dom";
+import Dashbord from "./components/dashbord/Dashbord";
+import Sidebar from "./components/sidebar/sidebar";
+import Layout from "./components/layouts/lauout";
+import Stu from "./pages/stu";
 
 // root component
 // create component using fucntions
@@ -14,12 +21,23 @@ import Home from "./pages/home";
 // first letter in the fucntion must be capital
 // JSX:
 
+// const [Students,setStudents] = useState([]);
 
 function App(){
+  const [students,setStudents] = useState([]);
   return(
     <div>
       {/* <Home/> */}
-      <Login/>
+      {/* <Login/> */}
+      <Routes>
+        <Route path="/home" element = {<Home/>}/>
+        <Route path="/login" element = {<Login/>}/>
+        <Route path="/registration" element = {<Register/>} />
+        <Route element = {<Layout/>}>
+          {/* <Route path="/dashboard" element = {<Dashbord/>}/> */}
+          <Route path="/" element = {<Stu students = {students}/>}/>
+        </Route>
+      </Routes>
     </div>
   )
 }
