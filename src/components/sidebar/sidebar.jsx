f0eb563d91f52f4037f2820201056acbf1356eed
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-10 12:53:25 +0530

    Added NavLink to the Navigation Bar V0.3

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,16 +1,59 @@
+import { Link, NavLink } from "react-router-dom";
 import "./sidebar.css"
 
 function Sidebar(){
     return(
         <div className="leftbar">
             <ul>
+                <NavLink to="/dashboard"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
                 <li>Dashboard</li>
+                </NavLink>
+                <NavLink to="/student"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
                 <li>Student</li>
+                </NavLink>
+                <NavLink to="/Companies"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
                 <li>Companies</li>
-                <li>Placements</li>
-                <li>Rports</li>
-                <li>Serttings</li>
+                </NavLink>
+                <NavLink to="drive"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
+                <li>Drive's</li>
+                </NavLink>
+                <NavLink to="report"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
+                <li>Reports</li>
+                </NavLink>
+                <NavLink to="settings"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
+                <li>Settings</li>
+                </NavLink>
+                <NavLink to="login"
+                className = {({isActive})=>
+                    isActive ? "active":""    
+                }
+                >
                 <li>Logout</li>
+                </NavLink>
             </ul>
         </div>
     )
