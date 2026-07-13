04d577f751371f57dd4a0385f06cdc6ca6252159
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-13 22:34:57 +0530

    page notfound error exception handled

---
+++ src/pages/pagenotfound/NotFound.jsx
@@ -0,0 +1,11 @@
+import "./notfound.css";
+function Notfound(){
+    return(
+    <div className="body">
+        <h1>404</h1>
+        <h4>Page not Found</h4>
+        <p>The page you are looking for is not found</p>
+    </div>
+    )
+}
+export default Notfound;
