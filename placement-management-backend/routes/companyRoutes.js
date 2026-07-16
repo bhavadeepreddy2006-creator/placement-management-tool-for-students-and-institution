13f1838133ceba1e11494a205f698d77a10fe1a2
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-16 19:31:29 +0530

    created router for company API

--- placement-management-backend/routes/companyRoutes.js
+++ placement-management-backend/routes/companyRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getCompany } from "../controllers/companyControllers";
+import { getCompany } from "../controllers/companyControllers.js";
 
 const router = express.Router();
 
