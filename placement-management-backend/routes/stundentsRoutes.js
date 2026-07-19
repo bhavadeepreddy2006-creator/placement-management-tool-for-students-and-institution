3caa466f94bc48632abd08b2c817a6cf9c774102
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-19 23:12:57 +0530

    new routes are declared for crud operations

--- placement-management-backend/routes/stundentsRoutes.js
+++ placement-management-backend/routes/stundentsRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { getStudents, getStudnetById, addstudent, UpdateStudent, deleteStudent } from "../controllers/studentsController.js";
+import { getStudents, getStudentsById, addstudent, UpdateStudent, deleteStudent } from "../controllers/studentsController.js";
 // Router object 
 const router = express.Router();
 
 router.get("/",getStudents);
-router.get("/:id",getStudnetById);
+router.get("/:id",getStudentsById);
 router.post("/",addstudent);
 router.put("/:id",UpdateStudent);
 router.delete("/:id",deleteStudent)
