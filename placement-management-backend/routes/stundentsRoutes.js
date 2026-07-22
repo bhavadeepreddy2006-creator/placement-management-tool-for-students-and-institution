ede45f48d9a03447d186f671eb978f2ec70fef7a
bhavadeepreddy2006-creator <bhavadeepreddy2006@gmail.com>
2026-07-22 23:01:44 +0530

    reciving image from frontend and sending it to controller

--- placement-management-backend/routes/stundentsRoutes.js
+++ placement-management-backend/routes/stundentsRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { getStudents, getStudentsById, addstudent, UpdateStudent, deleteStudent, searchstudents } from "../controllers/studentsController.js";
+import upload from "../middleware/upload.js";
 // Router object 
 const router = express.Router();
 
 router.get("/",getStudents);
 router.get("/:id",getStudentsById);
-router.post("/",addstudent);
+router.post("/",
+    // to receive on file as image
+    upload.single("image"),
+    addstudent);
 router.put("/:id",UpdateStudent);
 router.delete("/:id",deleteStudent)
 
